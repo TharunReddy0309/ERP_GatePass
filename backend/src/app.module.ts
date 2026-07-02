5de276e4d09d9963fa96d6b7f57ead288dddba69
TharunReddy0309 <bhumireddytharunreddy9@gmail.com>
2026-07-03 00:22:37 +0530

    Created Prisma Files and database module int backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { HostelModule } from './hostel/hostel.module';
 import { StudentModule } from './student/student.module';
 import { PassesModule } from './passes/passes.module';
 import { BlockedModule } from './blocked/blocked.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
     imports: [
@@ -11,7 +12,8 @@ import { BlockedModule } from './blocked/blocked.module';
         HostelModule,
         StudentModule,
         PassesModule ,
-        BlockedModule
+        BlockedModule,
+        DatabaseModule
     ],
 })
 export class AppModule {}
